e3d3aff201b45ef5ef021c6e381efde03c9ac2d4
rknLA <kevin@rkn.la>
2012-02-12 06:51:13 -0800

    Fix one more db issue.

--- models/boombox.rb
+++ models/boombox.rb
@@ -28,7 +28,7 @@ class Song
   property :boombox_id,       Integer,    :required => true
 end
 
-DataMapper.setup(:default, 'mysql://rknLA@localhost/silent_boombox')
+DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
